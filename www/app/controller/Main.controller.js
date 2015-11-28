47ccef4fb50b8d2019d8558578ed73d80cca645c
André Fiedler <SunboX@users.noreply.github.com>
2015-11-28 21:14:00 +0100

    formatting

--- www/app/controller/Main.controller.js
+++ www/app/controller/Main.controller.js
@@ -11,26 +11,26 @@ sap.ui.controller('app.controller.Main', {
                 price: 16,
                 deleted: false
             }, {
-                    quantity: 1,
-                    name: 'Oven Baked Potato Chips',
-                    price: 7.99,
-                    deleted: false
-                }, {
-                    quantity: 1,
-                    name: 'Death Wish Ground Coffee',
-                    price: 19.99,
-                    deleted: false
-                }, {
-                    quantity: 4,
-                    name: 'Croissants',
-                    price: 6,
-                    deleted: false
-                }, {
-                    quantity: 1,
-                    name: 'Tempo Tissues 30 Pack',
-                    price: 4.95,
-                    deleted: true
-                }],
+                quantity: 1,
+                name: 'Oven Baked Potato Chips',
+                price: 7.99,
+                deleted: false
+            }, {
+                quantity: 1,
+                name: 'Death Wish Ground Coffee',
+                price: 19.99,
+                deleted: false
+            }, {
+                quantity: 4,
+                name: 'Croissants',
+                price: 6,
+                deleted: false
+            }, {
+                quantity: 1,
+                name: 'Tempo Tissues 30 Pack',
+                price: 4.95,
+                deleted: true
+            }],
             totalPrice: 49.98
         });
         this.getView().setModel(viewModel, 'viewModel');
